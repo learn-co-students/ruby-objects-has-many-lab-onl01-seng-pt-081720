3dc96e077e6bb898959c6b91b4b2479e2fadd8f8
Christian Pardo <chrispardopersonal@gmail.com>
2020-09-15 17:57:57 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,17 @@
+class Post 
+  
+  attr_accessor :name, :author 
+  
+  @@all = [] 
+  
+  def initialize(name)
+    @name = name
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  
+end 
--- lib/song.rb
+++ lib/song.rb
@@ -14,5 +14,11 @@ class Song
     @@all
   end 
   
-  
+  def artist_name
+    if artist
+      self.artist.name 
+    else 
+      nil 
+    end 
+  end 
 end 
