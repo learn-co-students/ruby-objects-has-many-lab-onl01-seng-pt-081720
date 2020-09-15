41f1bffdeca6a9ec29dda2d0870be451544df71d
tjennings343 <tjennings343@gmail.com>
2020-09-15 16:36:44 -0400

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,11 +5,28 @@ class Artist
 
     def initialize(name)
         @name = name
-        @songs = []
+        @song = []
     end
- 
 
-      def songs
-        @songs
+    def songs
+      Song.all.select do |song|
+        song.artist == self
       end
+    end
+
+    def add_song(song)
+      @song << song
+      song.artist = self
+    end
+
+    def add_song_by_name(song_name)
+      song = Song.new(song_name)
+      add_song(song)
+    end
+
+    def self.song_count
+      Song.all.count
+    end
+
+    
 end
